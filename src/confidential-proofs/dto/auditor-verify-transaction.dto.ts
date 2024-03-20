4a3e2d80f1c36bedf9c2c93e735fe95af5059ab7
Eric Richardson <eric@polymesh.network>
2024-03-20 11:38:00 -0400

    fix: 🐛 proof generation would fail for some legs
    update SDK version to fix inconsistent leg ordering, which prevented
    some confidential legs from being affirmed by the sender

--- src/confidential-proofs/dto/auditor-verify-transaction.dto.ts
+++ src/confidential-proofs/dto/auditor-verify-transaction.dto.ts
@@ -5,6 +5,8 @@ export class AuditorVerifyTransactionDto {
   @ApiProperty({
     description:
       'The public key of the auditor to verify with. Any leg with a provided sender proof involving this auditor will be verified. The corresponding private must be present in the proof server',
+    type: 'string',
+    example: '0x7e9cf42766e08324c015f183274a9e977706a59a28d64f707e410a03563be77d',
   })
   @IsString()
   readonly auditorKey: string;
